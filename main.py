26ad55322018f9b63166b44e16b8e3a796763b25
kubuntuclaps <kubuntuclaps@protonmail.com>
2021-12-18 19:32:26 +0100

    Version 2.0 - Improved bypass

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import sys
 import re
 import time
 from colorama import Fore, Back, init
+import pyperclip as pc
+import requests
+
+def bypass(download_id):
+    bypass_request = requests.get("http://dlsft.com/callback/info.php?id=" + download_id)
+    channel = bypass_request.text.split("**")[0]
+    requests.post(f"http://dlsft.com/callback/?channel={channel}&id={download_id}&action=started")
+    requests.post(f"http://dlsft.com/callback/?channel={channel}&id={download_id}&action=completed")
 
 def main(executable):
-    download_id = re.sub("[^0-9]", "", executable)
+    download_id = re.sub("[^0-9]", "", executable) 
+    bypass(download_id)
     print(Fore.GREEN + "Your download link is: " + Fore.RESET + f"http://directdl.xyz/dm.php?id={download_id}")
+    print("Copied to clipboard")
+    pc.copy(f"http://directdl.xyz/dm.php?id={download_id}")
     print("\nClosing in 5 seconds...")
     time.sleep(5)
-    return 0
 
 # Start
 if __name__ == '__main__':
